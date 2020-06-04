fabb8b8f6a4f9757eb67732d72e3191f0d2a8471
rambohippo <jcmcgarry6@gmail.com>
2020-06-04 11:45:34 -0700

    Improve unit tests
    - Improve formatting in shorten_url_test.py
    - Add simple database unit tests

--- tests/database_test.py
+++ tests/database_test.py
@@ -1,15 +1,94 @@
+import os
+import sqlite3
 import unittest
 import shorten_url
 
-class database_test(unittest.TestCase):
+testdb_dir = 'database'
+testdb_path = testdb_dir + '/url_shortener.db'
+testdb_table = 'urls'
 
-    def test_db_select(self):
-        pass
 
+class DatabaseTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Create database directory if it does not exist
+        if not os.path.exists(testdb_dir):
+            os.makedirs(testdb_dir)
+
+        # Initialize database table if it does not exist
+        try:
+            command = 'CREATE TABLE IF NOT EXISTS {} (base_url text, base_protocol text, ' \
+                      + 'base_domain text, base_path text, short_url text, short_path text)'
+            command = command.format(testdb_table)
+            con = sqlite3.connect(testdb_path)
+            con.execute(command)
+            print('Executed the CREATE TABLE command:', command)
+            print('UnitTest Set Up Completed')
+        except sqlite3.Error:
+            print('Error while initializing database')
+            raise
+        finally:
+            if con:
+                con.close()
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            os.remove(testdb_path)
+            os.removedirs(testdb_dir)
+            print('UnitTest Tear Down completed')
+        except OSError as e:
+            print('The file {} could not be removed from the file system'.format(testdb_path))
+            print(e)
+
+    def test_db_check_if_exists_empty(self):
+        self.assertFalse(shorten_url.db_check_if_exists('https://www.google.com/testsearch'))
+
+    def test_db_check_if_exists_data(self):
+        testsite_info = ['https://www.w3schools.com/python/python_mysql_insert.asp',
+                         'https', 'www.w3schools.com', '/python/python_mysql_insert.asp',
+                         'https://www.w3schools.com/jrvmtxdpyi', '/jrvmtxdpyi']
+        shorten_url.db_insert(testsite_info[0], testsite_info[1], testsite_info[2],
+                              testsite_info[3], testsite_info[4], testsite_info[5],
+                              db_path=testdb_path)
+        self.assertTrue(shorten_url.db_check_if_exists(testsite_info[0], db_path=testdb_path))
 
     def test_db_insert(self):
-        pass
+        testsite_info = ['https://pythonspot.com/python-database-programming-sqlite-tutorial/',
+                         'https', 'pythonspot.com', '/python-database-programming-sqlite-tutorial/',
+                         'https://pythonspot.com/hwlpgtpudr', '/hwlpgtpudr']
+        shorten_url.db_insert(testsite_info[0], testsite_info[1], testsite_info[2],
+                              testsite_info[3], testsite_info[4], testsite_info[5],
+                              db_path=testdb_path)
+        self.assertTrue(shorten_url.db_check_if_exists(testsite_info[0], db_path=testdb_path))
 
+    def test_db_select_base_url(self):
+        testsite_info = ['https://pyvideo.org/europython-2014/writing-awesome-command-line-programs-in-python.html',
+                         'https', 'pyvideo.org', '/europython-2014/writing-awesome-command-line-programs-in-python.html',
+                         'https://pyvideo.org/qfeaoglgpi', '/qfeaoglgpi']
+        shorten_url.db_insert(testsite_info[0], testsite_info[1], testsite_info[2],
+                              testsite_info[3], testsite_info[4], testsite_info[5],
+                              db_path=testdb_path)
 
-    def test_db_check_if_exists(self):
-        pass
+        condition = "base_url='" + testsite_info[0] + "'"
+        record = shorten_url.db_select(db_path=testdb_path, table=testdb_table, where=condition)
+
+        for aa, bb in zip(testsite_info, record[0]):
+            self.assertEquals(aa, bb)
+
+    def test_db_select_short_url(self):
+        testsite_info = [
+            'https://www.dcrainmaker.com/2020/03/elites-sterzo-steering-riser-block-accessory-in-depth-review.html',
+            'https', 'www.dcrainmaker.com',
+            '/2020/03/elites-sterzo-steering-riser-block-accessory-in-depth-review.html',
+            'https://www.dcrainmaker.com/leqqvzktiw', '/leqqvzktiw']
+        shorten_url.db_insert(testsite_info[0], testsite_info[1], testsite_info[2],
+                              testsite_info[3], testsite_info[4], testsite_info[5],
+                              db_path=testdb_path)
+
+        condition = "short_url='" + testsite_info[4] + "'"
+        record = shorten_url.db_select(db_path=testdb_path, table=testdb_table, where=condition)
+
+        for aa, bb in zip(testsite_info, record[0]):
+            self.assertEquals(aa, bb)
--- tests/shorten_url_test.py
+++ tests/shorten_url_test.py
@@ -3,7 +3,8 @@ import unittest
 import sys
 import shorten_url
 
-class shorten_url_test(unittest.TestCase):
+
+class ShortenURLTest(unittest.TestCase):
     
     def test_parse_options_open_no_arg(self):
         sys.argv[1:] = ['--open']
@@ -11,7 +12,6 @@ class shorten_url_test(unittest.TestCase):
             self.assertRaises(ArgumentError, shorten_url.parse_options())
         
         self.assertEqual(ex.exception.code, 2)
-
 
     def test_parse_options_open_arg(self):
         url = 'https://www.google.com'
@@ -23,14 +23,12 @@ class shorten_url_test(unittest.TestCase):
         self.assertEqual(args.open, url)
         self.assertEqual(args.shorten, None)
 
-
     def test_parse_options_shorten_no_arg(self):
         sys.argv.append('--shorten')
         with self.assertRaises(SystemExit) as ex:
             self.assertRaises(ArgumentError, shorten_url.parse_options())
         
         self.assertEqual(ex.exception.code, 2)
-
 
     def test_parse_options_shorten_arg(self):
         url = 'https://www.google.com'
@@ -42,7 +40,6 @@ class shorten_url_test(unittest.TestCase):
         self.assertEqual(args.open, None)
         self.assertEqual(args.shorten, url)
 
-
     def test_parse_options_no_args(self):
         temp_args = sys.argv
         sys.argv = [sys.argv[0]]
@@ -51,7 +48,6 @@ class shorten_url_test(unittest.TestCase):
 
         self.assertEqual(args.open, None)
         self.assertEqual(args.shorten, None)
-
 
     def test_parse_options_open_and_shorten(self):
         url = 'https://www.google.com'
